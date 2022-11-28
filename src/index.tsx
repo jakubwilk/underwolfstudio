179b51aadfdb7a952f3d1dfdc8c7fbe994d61665
jakubwilk <jw@jakubwilk.pl>
2022-11-28 11:14:32 +0100

    Reorganize project codebase, change eslint settings and delete unecessary files

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { App } from '@app-domain'
 
-import App from './App'
 import reportWebVitals from './reportWebVitals'
 
-import './index.css'
+import '@app-style/index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
